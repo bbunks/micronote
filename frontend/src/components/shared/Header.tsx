24a2176ef3889a67b6c16c7a34dc96e9dc16caaf
Brenden Bunker <brendenbunker@gmail.com>
2023-10-31 09:08:40 -0600

    setup stores for tags

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -7,11 +7,14 @@ export function Header() {
   const currentSettings = useHeaderHook();
 
   return (
-    <div className="p-6 flex flex-row items-center h-header-height center-or-space">
-      {currentSettings.showLogo && <Logo />}
-      {currentSettings.centralElement && currentSettings.centralElement}
-      <Search />
-      <Button variant="PrimaryInverse">Profile</Button>
-    </div>
+    <>
+      <div className="p-6 flex flex-row items-center h-header-height center-or-space fixed top-0 left-0 right-0 bg-primary">
+        {currentSettings.showLogo && <Logo />}
+        {currentSettings.centralElement && currentSettings.centralElement}
+        <Search />
+        <Button variant="PrimaryInverse">Profile</Button>
+      </div>
+      <div className="h-header-height w-full" />
+    </>
   );
 }
--- frontend/src/stores/TagsStore.ts
+++ frontend/src/stores/TagsStore.ts
@@ -1,32 +1,29 @@
 import { Watcher } from "wal.js";
 import { useWatcherState } from "react-state-extended";
-import { useEffect } from "react";
 
 export const tagsWatcher = new Watcher<Tag[]>([]);
 
 let nextUpdateTime: Date;
 
 function updateTags() {
+  if (nextUpdateTime === undefined || nextUpdateTime.getTime() < Date.now()) {
+    const s = new Date();
+    s.setMinutes(s.getMinutes() + 1);
+    nextUpdateTime = s;
+
+    tagsWatcher.value = [
+      { label: "Hello", id: 1, color: "#000" },
+      { label: "World", id: 2, color: "#000" },
+      { label: "You", id: 3, color: "#000" },
+      { label: "are", id: 4, color: "#000" },
+      { label: "Great", id: 5, color: "#000" },
+    ];
+  }
   // TODO: fetch data from an api
-  tagsWatcher.value = [
-    { label: "Hello", id: 1, color: "#000" },
-    { label: "World", id: 2, color: "#000" },
-    { label: "You", id: 3, color: "#000" },
-    { label: "are", id: 4, color: "#000" },
-    { label: "Great", id: 5, color: "#000" },
-  ];
 }
 
 export function useTags() {
-  useEffect(() => {
-    if (nextUpdateTime === undefined || nextUpdateTime.getTime() < Date.now()) {
-      const s = new Date();
-      s.setMinutes(s.getMinutes() + 1);
-      nextUpdateTime = s;
-
-      updateTags();
-    }
-  }, []);
+  updateTags();
 
   return useWatcherState<Tag[]>(tagsWatcher)[0];
 }
