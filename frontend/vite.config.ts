a8462a4292e320c294403fa8ea20715486b63e5f
Brenden Bunker <brendenbunker@gmail.com>
2023-11-14 14:08:31 -0700

    added vite build to output to the backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-      "/auth": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    build: {
+      outDir:
+        env.NODE_ENV === "production"
+          ? "../backend/src/main/resources/static"
+          : "./dist",
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+        "/auth": {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
+  };
 });
