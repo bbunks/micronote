754e38801870f1254d2c5badbb4f12525b2b6143
Brenden Bunker <brendenbunker@gmail.com>
2023-11-14 19:37:20 -0700

    trying to publish github container

--- SearchQuery.sql
+++
@@ -1,8 +0,0 @@
-USE micronote;
-
-SELECT *
-FROM micronote.note n
-    JOIN micronote.note_content nc on n.id = nc.note_id
-    JOIN micronote.note_tag nt on n.id = nt.note_id
-#     JOIN micronote.tag t on t.id = nt.tag_id
-WHERE nt.tag_id in (10, 11)
