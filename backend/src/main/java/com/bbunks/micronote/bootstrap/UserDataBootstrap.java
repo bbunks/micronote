ad2d73ab733ddae3bc2531816743917865f90bb4
Brenden Bunker <brendenbunker@gmail.com>
2024-10-23 01:54:55 -0700

    Added first user generation if in dev

--- backend/src/main/java/com/bbunks/micronote/bootstrap/UserDataBootstrap.java
+++ backend/src/main/java/com/bbunks/micronote/bootstrap/UserDataBootstrap.java
@@ -1,25 +1,39 @@
 package com.bbunks.micronote.bootstrap;
 
-import com.bbunks.micronote.entities.User;
-import com.bbunks.micronote.services.UserService;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.bbunks.micronote.entities.User;
+import com.bbunks.micronote.services.UserService;
 
 @Component
 public class UserDataBootstrap implements CommandLineRunner {
+
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    Environment env;
+
     @Override
     public void run(String... args) {
-//        if (userService.getCount() == 0) {
-//            User brenden = new User();
-//            brenden.setFirstName("Brenden");
-//            brenden.setLastName("Bunker");
-//            brenden.setEmail("brendenbunker@gmail.com");
-//            brenden.setPassword("$2y$10$rpldcsKqDy3SFoJOISKbr.Z9dBw/KBDIi0r.4bR71DQytiLtjkj8q");
-//            userService.save(brenden);
-//        }
+        String[] activeProfiles = env.getActiveProfiles();
+
+        if (userService.getCount() == 0 && Arrays.stream(activeProfiles).anyMatch("dev"::equals)) {
+            User brenden = new User();
+            brenden.setFirstName("Brenden");
+            brenden.setLastName("Bunker");
+            brenden.setEmail("brendenbunker@gmail.com");
+            brenden.setPassword(encoder.encode("TestPassword"));
+            userService.save(brenden);
+        }
     }
 }
